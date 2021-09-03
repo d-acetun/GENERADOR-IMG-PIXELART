4c0dc43784c6a00f8e6b4b7c4449f9ac9a787cf0
DiegoAcetun <diegoacetun00345@gmail.com>
2021-09-02 20:57:22 -0600

    Se pusieron colores a los botones

--- python/main.py
+++ python/main.py
@@ -1,5 +1,7 @@
 from tkinter import *
-import tkinter
+import tkinter as tk
+from tkinter import font
+from tkinter.font import Font
 import easygui
 def funcion():
     raiz.destroy()
@@ -22,38 +24,39 @@ def Cargar():
                     
                 
 
-raiz = Tk()
+raiz = tk.Tk()
 raiz.title("Bitxelart")
 raiz.geometry("1250x750+150+10")
-BCargar = tkinter.Button(raiz, text="Cargar Archivo", command=Cargar, height=2, width=15)
+
+BCargar = tk.Button(raiz, text="Cargar Archivo", command=Cargar, height=2, width=15, bg="midnightblue", fg="white")
 BCargar.pack()
 BCargar.place(x=10, y=10)
 
-BAnalizar = tkinter.Button(raiz, text="Analizar", command=funcion, height=2, width=15)
+BAnalizar = tk.Button(raiz, text="Analizar", command=funcion, height=2, width=15, bg="darkgreen", fg="white")
 BAnalizar.pack()
 BAnalizar.place(x=122, y=10)
 
-BReportes = tkinter.Button(raiz, text="Reportes", command=funcion, height=2, width=15)
+BReportes = tk.Button(raiz, text="Reportes", command=funcion, height=2, width=15, bg="indigo", fg="white")
 BReportes.pack()
 BReportes.place(x=232, y=10)
 
-BSalir = tkinter.Button(raiz, text="Salir", command=funcion, height=2, width=15)
+BSalir = tk.Button(raiz, text="Salir", command=funcion, height=2, width=15, bg="orange red", fg="white")
 BSalir.pack()
 BSalir.place(x=342, y=10)
 
-BOriginal = tkinter.Button(raiz, text="Original", command=funcion, height=2, width=15)
+BOriginal = tk.Button(raiz, text="Original", command=funcion, height=2, width=15, bg="lightyellow", fg="black")
 BOriginal.pack()
 BOriginal.place(x=40, y=200)
 
-BMirrorX = tkinter.Button(raiz, text="Mirror X", command=funcion, height=2, width=15)
+BMirrorX = tk.Button(raiz, text="Mirror X", command=funcion, height=2, width=15, bg="steelblue", fg="white")
 BMirrorX.pack()
 BMirrorX.place(x=40, y=270)
 
-BMirrorY = tkinter.Button(raiz, text="Mirror Y", command=funcion, height=2, width=15)
+BMirrorY = tk.Button(raiz, text="Mirror Y", command=funcion, height=2, width=15, bg="maroon", fg="white")
 BMirrorY.pack()
 BMirrorY.place(x=40, y=340)
 
-BDoubleMirror = tkinter.Button(raiz, text="Double Mirror", command=funcion, height=2, width=15)
+BDoubleMirror = tk.Button(raiz, text="Double Mirror", command=funcion, height=2, width=15, bg="crimson", fg="white")
 BDoubleMirror.pack()
 BDoubleMirror.place(x=40, y=410)
 
